c99d208da7337928f2a77aed0c51315018780032
JGuillaumin <julien.guillaumin@telecom-bretagne.eu>
2017-08-15 13:12:48 +0200

    inputs

--- run_project.py
+++ run_project.py
@@ -16,7 +16,7 @@ print("\n===============\n")
 os.system("pwd")
 time.sleep(1)
 print("\n===============\n")
-os.system("ls -la /valohai/*")
+os.system("ls -la /valohai/inputs/*")
 time.sleep(1)
 print("\n===============\n")
 
@@ -25,7 +25,7 @@ if os.path.isfile("main.py"):
 else:
     print("main.py not found")
 
-if os.path.isfile("/valohai/inputs/main.py"):
-    os.system("python /valohai/inputs/main.py")
+if os.path.isfile("/valohai/inputs/main_script/main2.py"):
+    os.system("python /valohai/inputs/main_script/main2.py")
 else:
-    print("/valohai/inputs/main.py not found")
+    print("/valohai/inputs/main_script/main2.py not found")
